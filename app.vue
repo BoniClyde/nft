cd559058cf47227a91ecbfaa11b173fdba9ebeba
BoniClyde <118424436+BoniClyde@users.noreply.github.com>
2023-05-20 13:53:39 +0100

    structured folder

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-red-500 p-44">
     <NuxtWelcome />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   css: ["~/assets/main.css","~/assets/fa/css/all.min.css"],
   modules: [
     '@nuxtjs/tailwindcss',
@@ -8,6 +9,8 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true
   },
+
+  
 
   
 })
