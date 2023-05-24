ff7772d35980b7b0c6d61ac42d1552ee49b72a79
BoniClyde <118424436+BoniClyde@users.noreply.github.com>
2023-05-24 22:35:47 +0100

    login 90%

--- pages/login.vue
+++ pages/login.vue
@@ -32,15 +32,17 @@
 
               <div>
                 <label for="password" class="heading">Password</label>
-                <div class="mt-2">
+                <div class="mt-2"></div>
+                <div class="relative mt-2 flex items-center">
                   <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autocomplete="current-password"
-                    required=""
-                    class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type="text"
+                    name="search"
+                    id="search"
+                    class="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  <div class="absolute inset-y-0 right-0 flex py-2 pr-1.5">
+                    <i class="fa-solid fa-eye-slash"></i>
+                  </div>
                 </div>
               </div>
 
