f9f1e036407b44c03764f6898bef48feedfd19b2
alligator33 <>
2023-05-24 02:44:58 +0100

    everything working

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -68,6 +68,8 @@
         </div>
       </div>
 
+
+      
       <div
         v-if="showMobileMenu"
         class="absolute right-0 h-52 w-48 border bg-white font-semibold transition-opacity duration-500"
