986d2d09c5d3db9e81d68bc79b654da4feff51e3
BoniClyde <118424436+BoniClyde@users.noreply.github.com>
2023-05-29 18:33:55 +0100

    Update TrendingSection.vue

--- layouts/TrendingSection.vue
+++ layouts/TrendingSection.vue
@@ -61,7 +61,7 @@
         class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
       >
         <li
-          class="gradient relative rounded-2xl"
+          class="relative rounded-2xl bg-primary-900"
           v-for="(item, index) in nfts"
           :key="index"
         >
