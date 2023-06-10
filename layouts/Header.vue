0891c90229ff226c0e52ddb6c35ebcd843273e27
alligator33 <>
2023-06-10 14:43:26 +0100

    added search

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -17,7 +17,11 @@
               placeholder="Search for collections, NFTs or users"
               class="hidden rounded-md bg-gray-100 px-6 py-2 text-gray-900 sm:w-80 md:w-80 lg:block"
             />
-            <button @click="toggleSearchBar" class="lg:hidden">
+            <button
+              v-if="$route.path !== '/explore'"
+              @click="toggleSearchBar"
+              class="lg:hidden"
+            >
               <i
                 class="fa-solid fa-magnifying-glass theme-text transition-all duration-300 hover:scale-105"
               ></i>
@@ -102,13 +106,11 @@
       <!-- Mobile -->
 
       <div
-        v-if="showSearch"
+        v-if="showSearch && $route.path !== '/explore'"
         id="searchBar area"
         class="glass fixed top-0 z-50 h-24 w-full bg-white py-5 dark:bg-secondary-900"
       >
-        <div
-          class="bg-red-40 grid grid-cols-4 items-center justify-between px-6 pt-1"
-        >
+        <div class="grid grid-cols-4 items-center justify-between px-6 pt-1">
           <div class="col-span-3 transition-all delay-100 duration-300">
             <input
               type="text"
@@ -190,6 +192,7 @@ function gotToSearch() {
     $router.push({
       path: "/explore",
     });
+    toggleSearchBar();
   } else {
     console.log("you typed nothing");
   }
--- pages/explore.vue
+++ pages/explore.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="container mx-auto lg:px-8">
     <div class="py-10 text-center text-xl font-bold">Explore NFTs</div>
-    <div v-if="pending"></div>
+
+    {{ search_store.searchQuery }}
+    <div class="flex justify-center">
+      {{ selectedType }}
+      <input
+        id="searchInput"
+        v-model="search_store.searchQuery"
+        type="text"
+        placeholder="Search for collections, NFTs or users"
+        class="w-4/5 rounded-md bg-gray-100 px-6 py-2 text-gray-900"
+      />
+    </div>
+    <div v-if="pending">Loading ....</div>
 
     <template v-else>
       <div v-if="data?.data.length > 0">
@@ -60,7 +72,7 @@
             class="mx-auto mt-20 grid grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
           >
             <li
-              class="image-container relative cursor-pointer overflow-hidden rounded-2xl border-4 shadow dark:border-6 border-white dark:border-secondary-900"
+              class="image-container dark:border-6 relative cursor-pointer overflow-hidden rounded-2xl border-4 border-white shadow dark:border-secondary-900"
               v-for="(item, index) in data?.data"
               :key="index"
             >
@@ -81,7 +93,7 @@
               />
 
               <div
-                class="theme-text absolute bottom-0 z-40 w-full bg-white dark:bg-secondary-900 p-4"
+                class="theme-text absolute bottom-0 z-40 w-full bg-white p-4 dark:bg-secondary-900"
               >
                 <h3 class="text-lg font-semibold leading-8 tracking-tight">
                   <TruncateString :value="item.collectionName" :length="20" />
@@ -128,9 +140,15 @@ import TruncateString from "~/components/utils/TruncateString.vue";
 import { serverUrl } from "~/app.config";
 import { searchStore } from "~/store/appStore";
 
+
+
+
 const selectedType = ref<"nft" | "collection">("nft");
 
 const search_store = searchStore();
+
+
+// search_store.searchQuery = "";
 
 function selectCollection() {
   selectedType.value = "collection";
@@ -149,23 +167,32 @@ const { data, pending, refresh } = await useAsyncData<{
   () =>
     $fetch(`${serverUrl}/nfts/all-nfts`, {
       params: {
-        perPage: 15,
+        perPage: 50,
         search: search_store.searchQuery,
         //  search: "chum",
         type: selectedType.value,
       },
     }),
+
   {
     // watch: [selectedType, search_store.searchQuery],
-    watch: [selectedType],
+    watch: [selectedType, search_store],
   }
 );
+
+
+/* watch(() => search_store.searchQuery, () => {
+console.log("search_store.searchQuery", search_store.searchQuery);
+}
+); */
+
 </script>
 
 <style scoped>
 .active {
   @apply border-b-2 border-primary-500 px-2;
 }
+/* search_store.searchQuery = ""; */
 
 .inactive {
   @apply hover:border-secondary-500;
