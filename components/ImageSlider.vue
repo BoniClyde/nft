af7159427fa27c7a2eda243632b675d5d86e268b
alligator33 <>
2023-06-12 10:15:23 +0100

    removed package

--- components/ImageSlider.vue
+++ components/ImageSlider.vue
@@ -40,15 +40,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-@import "@/styles/variables.scss";
-@import "@/styles/mixins.scss";
-@import "./style.scss";
 
-.swiper {
-  @include swiper-wrapper();
-}
 
-.slide {
-  @include swiper-slide();
-}
 </style>
