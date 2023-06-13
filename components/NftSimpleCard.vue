803a60114d3470f6daddd086a8be8a5e690d773e
BoniClyde <118424436+BoniClyde@users.noreply.github.com>
2023-06-13 20:23:09 +0100

    sliders working

--- components/NftSimpleCard.vue
+++ components/NftSimpleCard.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <li class="relative rounded-2xl bg-primary-900">
-      <img
+    <li
+      class="image-container relative cursor-pointer overflow-hidden rounded-2xl border-4 shadow dark:border-secondary-900"
+    >
+      <!-- <img
         class="aspect-[14/13] w-full rounded-t-2xl object-cover"
         :src="src"
         alt=""
+      /> -->
+      <nuxt-img
+        class="aspect-[3/4] w-full rounded-t-2xl object-cover"
+        sizes="sm:100vw md:50vw lg:400px"
+        :src="src"
+        @error="() => (item.collectionImage = '/nft/defaultErrorImage.png')"
+        placeholder="nft/deafaultErrorImage.png"
       />
-
-      <div class="p-4">
-        <h3
-          class="mt-6 text-lg  leading-8 tracking-tight text-gray-50"
-        >
+      <!-- <div class="p-4">
+        <h3 class="mt-6 text-lg leading-8 tracking-tight text-gray-50">
           <TruncateString :value="name" :length="20" />
         </h3>
         <div class="flex justify-between">
@@ -18,6 +24,22 @@
           <p class="text-base font-normal leading-6 text-gray-200">
             {{ price }} ETH
           </p>
+        </div>
+      </div> -->
+      <div
+        class="theme-text absolute bottom-0 z-40 w-full bg-white p-4 dark:bg-secondary-900"
+      >
+        <div class="flex items-center gap-x-2">
+          <TruncateString
+            class="text-lg font-semibold leading-8 tracking-tight"
+            :value="name"
+            :length="20"
+          />
+        </div>
+
+        <div class="flex justify-between">
+          <p>Floor Price</p>
+          <p class="text-base font-medium leading-6">{{ price }} ETH</p>
         </div>
       </div>
     </li>
@@ -66,5 +88,14 @@ function onImageError() {
   to {
     background-position: 2200px 0;
   }
+}
+
+.image-container:hover img {
+  transform: scale(1.2);
+  transform-origin: center center;
+}
+
+.image-container img {
+  transition: transform 0.3s ease;
 }
 </style>
--- pages/section/TestimonialSlider.vue
+++ pages/section/TestimonialSlider.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import { register } from "swiper/element/bundle";
+import TestimonialCard from "~/layouts/TestimonialCard.vue";
+import { Testimonials } from "~/types/model";
 
 register();
 onMounted(() => {
@@ -18,17 +20,21 @@ onMounted(() => {
     },
     loop: true,
     breakpoints: {
-      640: {
-        slidesPerView:3,
+      480: {
+        slidesPerView: 1,
         spaceBetween: 30,
       },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
       1024: {
-        slidesPerView: 4,
-        spaceBetween: 30,
+        slidesPerView: 1,
+        spaceBetween: 50,
       },
     },
     autoplay: {
-      delay: 100,
+      delay: 1600,
       disableOnInteraction: false,
     },
     navigation: false,
@@ -39,13 +45,61 @@ onMounted(() => {
   swiperEl.initialize();
   console.log(swiperEl);
 });
+const testimonials = ref<Testimonials[]>([
+  {
+    name: "Jane Smith",
+    position: "Marketing Manager",
+    image: "/nft/nft1.png",
+    description:
+      "John Doe is an exceptional leader with a clear vision. His dedication and expertise have greatly contributed to the success of our company. It's a pleasure working under his guidance.",
+    star: 5,
+  },
+  {
+    name: "Michael Johnson",
+    position: "Sales Representative",
+    image: "/nft/nft2.png",
+    description:
+      "Working with John Doe has been a wonderful experience. His ability to motivate and inspire the team is truly remarkable. I'm grateful for the opportunity to learn from him.",
+    star: 4,
+  },
+  {
+    name: "Emily Williams",
+    position: "Product Designer",
+    image: "/nft/nft3.png",
+    description:
+      "John Doe is a creative thinker who always encourages innovation. His attention to detail and commitment to quality have greatly influenced my approach to design. I highly recommend him as a leader.",
+    star: 5,
+  },
+  {
+    name: "David Thompson",
+    position: "Finance Manager",
+    image: "/nft/nft4.png",
+    description:
+      "I've had the privilege of working closely with John Doe on several projects. His strategic mindset and problem-solving abilities have been instrumental in achieving our financial goals. He is a true asset to our organization.",
+    star: 4,
+  },
+  {
+    name: "Sophia Davis",
+    position: "Human Resources Specialist",
+    image: "/nft/nft5.png",
+    description:
+      "John Doe's leadership style fosters a positive and inclusive work environment. His genuine care for the well-being of employees has helped create a strong team dynamic. It's a pleasure being part of his team.",
+    star: 5,
+  },
+]);
 </script>
 <template>
   <div class="px-10 md:px-0">
     <swiper-container id="testify-slider" class="mySwiper" :init="false">
-      <swiper-slide v-for="(item, index) in 10" :key="index" class="h-80">{{
-        index
-      }}</swiper-slide>
+      <swiper-slide v-for="(item, index) in testimonials" :key="index">
+        <TestimonialCard
+          :name="item.name"
+          :description="item.description"
+          :position="item.position"
+          :star="item.star"
+          :image="item.image"
+        />
+      </swiper-slide>
     </swiper-container>
   </div>
 </template>
@@ -53,13 +107,11 @@ onMounted(() => {
 <style scoped>
 swiper-container {
   width: 100%;
-  height: 100%;
 }
 
 swiper-slide {
   text-align: center;
   font-size: 18px;
-  background: red;
   display: flex;
   justify-content: center;
   align-items: center;
