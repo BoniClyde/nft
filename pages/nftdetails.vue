782ffce14795b974d235c8c692af88bcdeba9d74
BoniClyde <118424436+BoniClyde@users.noreply.github.com>
2023-06-19 08:36:32 +0100

    nft details page 20%

--- pages/nftdetails.vue
+++ pages/nftdetails.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container mx-auto p-4">
+    <div>
+      <h1 class="theme-text heading mb-4">Collection Name</h1>
+
+      <h2 class="theme-text text-3xl">NFT 8 - Death of a Salesman</h2>
+    </div>
     <div class="grid grid-cols-1 gap-8 md:grid-cols-2">
       <div class="space-y-2 py-4">
         <div
@@ -29,11 +34,89 @@
       </div>
 
       <div>
-        <h1 class="theme-text heading">Collection Name</h1>
+        <div class="mb-6">Death of a Salesman</div>
+        <div class="mb-6 flex flex-row gap-x-4">
+          <button class="btn2">1.49ETH</button>
+          <button class="btn1">$3,500</button>
+        </div>
+        <div class="mb-6">
+          A masterpiece built with vibrant colors that represents the freedom of
+          womanhood. This art also represents intersectional self as well. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+        </div>
+        <div class="mb-6 flex gap-x-4">
+          <img class="h-14 w-14 rounded-full" src="nft/nft3.png" alt="" />
+          <div class="">
+            <h1>Adem Smith</h1>
+            <p>By Owner</p>
+          </div>
+        </div>
+        <div class="mb-6">
+          <div>
+            <div class="mb-4"><h1>Latest Bids</h1></div>
+            <div class="flow-root">
+              <ul>
+                <li v-for="(event, eventIdx) in timeline" :key="event.id">
+                  <div class="relative pb-8">
+                    <!-- <span
+                      v-if="eventIdx !== timelines.length - 1"
+                      class="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200"
+                      aria-hidden="true"
+                    /> -->
+                    <div class="relative flex space-x-6">
+                      <div>
+                        <span
+                          :class="[
+                            event.iconBackground,
+                            'flex h-8 w-8 items-center justify-center rounded-full ring-8 ring-white',
+                          ]"
+                        >
+                          <component
+                            :is="event.icon"
+                            class="h-5 w-5 text-white"
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </div>
+                      <div
+                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5"
+                      >
+                        <div>
+                          <p class="text-sm text-gray-500">
+                            {{ event.content }}
+                            <a
+                              :href="event.href"
+                              class="font-medium text-gray-900"
+                              >{{ event.target }}</a
+                            >
+                          </p>
+                        </div>
+                        <div
+                          class="whitespace-nowrap text-right text-sm text-gray-500"
+                        >
+                          <time :datetime="event.datetime">{{
+                            event.date
+                          }}</time>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="mb-6 flex flex-row gap-x-4">
+          <button class="btn">Place a bid</button>
+          <button class="btn2">Purchase Now</button>
+        </div>
+      </div>
+      <div v-if="false">
+        <h1 class="theme-text heading mb-4">Collection Name</h1>
 
-        <h2 class="theme-text heading">NFT 8 - Death of a Salesman (NFT)</h2>
+        <h2 class="theme-text text-3xl">NFT 8 - Death of a Salesman</h2>
 
-        <div class="flex justify-between py-4">
+        <div class="mb-4 flex justify-between py-4">
           <div class="flex gap-x-4">
             <div>
               <img class="h-14 w-14 rounded-full" src="nft/nft3.png" alt="" />
@@ -53,7 +136,43 @@
             </div>
           </div>
         </div>
-        <div class="0 mx-auto border border px-4">
+        <div class="mb-2 border-b-2 border-gray-100"></div>
+        <div class="flex justify-between">
+          <div class="flex gap-x-2">
+            <p>1</p>
+            <i class="fa-sharp fa-regular fa-heart"></i>
+          </div>
+          <div class="flex gap-x-2">
+            <p>Share</p>
+            <i class="fa-sharp fa-regular fa-share-from-square"></i>
+          </div>
+
+          <div class="flex gap-x-2">
+            <p>Refresh</p>
+            <i class="fa-duotone fa-arrows-rotate"></i>
+          </div>
+        </div>
+        <div>
+          <div class="bg-blue-500 p-10">
+            <h1>Price things</h1>
+            <h2>Price</h2>
+            <h3>In Dollar</h3>
+          </div>
+          <div class="bg-red-400 p-10">
+            <h1>Last Sale Price</h1>
+            <div>
+              <button>Buy now</button>
+            </div>
+            <div class="">
+              <button>Place bid</button>
+            </div>
+          </div>
+          <div class="bg-green-400 p-10">
+            <p>Countdown</p>
+          </div>
+        </div>
+
+        <!-- <div class="0 mx-auto px-4">
           <div class="flex flex-col gap-4 p-4">
             <div>
               <h3 class="text-lg font-semibold">Offers</h3>
@@ -81,7 +200,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
 
@@ -92,47 +211,55 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
-import NftSimpleSlider from "./section/NftSimpleSlider.vue";
+import CollectionSlider from "~/pages/section/CollectionSlider.vue";
 
-const props = defineProps({
-  name: {
-    type: String,
-    required: true,
+const timeline = [
+  {
+    id: 1,
+    content: "Applied to",
+    target: "Front End Developer",
+    href: "#",
+    date: "Sep 20",
+    datetime: "2020-09-20",
+    icon: "fa-duotone fa-user",
   },
-  src: {
-    type: String,
-    required: false,
+  {
+    id: 2,
+    content: "Advanced to phone screening by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 22",
+    datetime: "2020-09-22",
+    icon: "fa-duotone fa-thumbs-up",
   },
-  price: {
-    type: Number,
-    required: false,
+  {
+    id: 3,
+    content: "Completed phone screening with",
+    target: "Martha Gardner",
+    href: "#",
+    date: "Sep 28",
+    datetime: "2020-09-28",
+    icon: "fa-solid fa-check",
   },
-  listings: {
-    type: Number,
-    required: false,
+  {
+    id: 4,
+    content: "Advanced to interview by",
+    target: "Bethany Blake",
+    href: "#",
+    date: "Sep 30",
+    datetime: "2020-09-30",
+    icon: "fa-duotone fa-thumbs-up",
   },
-  offers: {
-    type: Number,
-    required: false,
+  {
+    id: 5,
+    content: "Completed interview with",
+    target: "Katherine Snyder",
+    href: "#",
+    date: "Oct 4",
+    datetime: "2020-10-04",
+    icon: "fa-solid fa-check",
   },
-  contractAddress: {
-    type: String,
-    required: false,
-  },
-  tokenId: {
-    type: String,
-    required: false,
-  },
-  chain: {
-    type: String,
-    required: false,
-  },
-  metadata: {
-    type: String,
-    required: false,
-  },
-});
+];
 </script>
 
 <style scoped>
--- pages/section/CollectionSlider.vue
+++ pages/section/CollectionSlider.vue
@@ -1,71 +1,12 @@
 <template>
   <div>
-    <h1>Native slider</h1>
-    <Splide
-      :has-track="false"
-      :options="options"
-      aria-label="My Favorite Images"
-      class=""
-    >
-      <SplideTrack>
-        <SplideSlide v-for="(item, index) in imgData" :key="index">
-          <img class="h-64" :src="item" alt="Sample 1" />
-        </SplideSlide>
-      </SplideTrack>
-      <button class="splide__toggle" type="button">
-        <span class="splide__toggle__play">Play</span>
-        <span class="splide__toggle__pause">Pause</span>
-      </button>
-      <div class="splide__progress">
-        <div class="splide__progress__bar"></div>
-      </div>
+    <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
+      <SplideSlide>
+        <img src="nft/nft2.png" alt="Sample 1" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src="image2.jpg" alt="Sample 2" />
+      </SplideSlide>
     </Splide>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { Splide, SplideSlide, SplideTrack } from "@splidejs/vue-splide";
-import { defineComponent } from "vue";
-
-import "@splidejs/vue-splide/css";
-
-// or other themes
-import "@splidejs/splide/css/skyblue";
-
-import "@splidejs/vue-splide/css/core";
-
-const imgData = [
-  "nft/nft1.png",
-  "nft/nft2.png",
-  "nft/nft3.png",
-  "nft/nft4.png",
-  "nft/nft5.png",
-  "nft/nft6.png",
-  "nft/nft7.png",
-  "nft/nft8.png",
-  "nft/nft9.png",
-  "nft/nft10.png",
-  "nft/nft11.png",
-  "nft/nft12.png",
-];
-const options = {
-  rewind: true,
-  gap: "3rem",
-  type: "loop",
-  perPage: 4,
-  pagination: false,
-  arrows: false,
-  breakpoints: {
-    1200: { perPage: 2, gap: "1rem" },
-    640: { gap: 0 },
-  },
-  autoplay: true,
-};
-</script>
-
-<style lang="scss" scoped>
-.splide__progress__bar {
-  height: 3px;
-  @apply bg-primary-500;
-}
-</style>
