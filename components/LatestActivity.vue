2f34177f38bdc4bff08274e115eba15c021dcb59
alligator33 <>
2023-06-23 04:28:20 +0100

    infinite scroll

--- components/LatestActivity.vue
+++ components/LatestActivity.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-      <ul>{{ items }}
-        <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+      <ul>{{ items.length }}
+import Image from './utils/Image.vue';
+        <li v-for="item in items" :key="item.id">
+            {{ item }}
+        
+        
+        <img
+
+        :src="item.media.gateway"
+        
+
+
+        />
+        </li>
       </ul>
       <div ref="scrollTrigger"></div>
     </div>
@@ -9,55 +21,57 @@
   
   <script>
   import axios from 'axios';
-  
+  import Image from './utils/Image.vue';
   export default {
+    components: {
+      Image,
+    },
     data() {
-      return {
-        items: [],
-        page: 1,
-        perPage: 3,
-        loading: false,
-      };
+        return {
+            items: [],
+            page: 1,
+            perPage: 3,
+            loading: false,
+        };
     },
     mounted() {
-      this.fetchData();
-      this.setupScrollListener();
+        this.fetchData();
+        this.setupScrollListener();
     },
     methods: {
-      fetchData() {
-        if (this.loading) return;
-        this.loading = true;
-  
-        axios
-          .get(`http://localhost:5066/api/nfts/all-nfts?perPage=${this.perPage}&page=${this.page}`)
-          .then(response => {
-            console.log(response.data.data);
-            this.items = [...this.items, ...response.data.data];
-            this.page++;
-            this.loading = false;
-          })
-          .catch(error => {
-            console.error(error);
-            this.loading = false;
-          });
-      },
-      setupScrollListener() {
-        const scrollTrigger = this.$refs.scrollTrigger;
-        const options = {
-          rootMargin: '0px',
-          threshold: 1.0,
-        };
-  
-        const observer = new IntersectionObserver(entries => {
-          if (entries[0].isIntersecting) {
-            console.log('triggered');
-            this.fetchData();
-          }
-        }, options);
-  
-        observer.observe(scrollTrigger);
-      },
+        fetchData() {
+            if (this.loading)
+                return;
+            this.loading = true;
+            axios
+                .get(`http://localhost:5066/api/nfts/all-nfts?perPage=${this.perPage}&page=${this.page}`)
+                .then(response => {
+                console.log(response.data.data);
+                this.items = [...this.items, ...response.data.data];
+                this.page++;
+                this.loading = false;
+            })
+                .catch(error => {
+                console.error(error);
+                this.loading = false;
+            });
+        },
+        setupScrollListener() {
+            const scrollTrigger = this.$refs.scrollTrigger;
+            const options = {
+                rootMargin: "0px",
+                threshold: 1,
+            };
+            const observer = new IntersectionObserver(entries => {
+                if (entries[0].isIntersecting) {
+                    console.log("triggered");
+                    this.fetchData();
+                }
+            }, options);
+            observer.observe(scrollTrigger);
+        },
     },
-  };
+    components: { Image }
+};
   </script>
   
