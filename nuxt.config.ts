6a33bff12d71876ced1d39f91d3656b4546617f5
alligator33 <>
2023-06-24 14:32:59 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
     pageTransition: { name: "page", mode: "out-in" },
     layoutTransition: { name: "layout", mode: "out-in" },
   },
+ 
   css: [
     "~/assets/main.css",
     "~/assets/fa/css/all.min.css",
