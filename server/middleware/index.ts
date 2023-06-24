ac4e68ca11a46be5a50b28498948c7427e280e1c
alligator33 <>
2023-06-24 05:17:29 +0100

    Update index.ts

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -1,9 +1,8 @@
-/* export default defineEventHandler((event:any) => {
+export default defineEventHandler((event:any) => {
     console.log('New request --- jjj: ' + getRequestURL(event))
 
-    // sets the Referrer-Policy header.
 
     event.response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
 
     
-  }) */
+  })
