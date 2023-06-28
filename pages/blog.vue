379293fcbece201bdf25597a7f3e3f3fda136d63
alligator33 <>
2023-06-28 16:29:15 +0100

    Update blog.vue

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,19 +1,37 @@
 <script lang="ts" setup>
 import PreLoader from "@/components/utils/PreLoader.vue";
- definePageMeta({
+definePageMeta({
   name: "blog",
-}); 
+});
 
 import axios from "axios";
+import Image from "../components/utils/Image.vue";
 
-const blogPosts = ref([]);
+type blogPostType = {
+  id: number;
+  title: { rendered: string };
+  _embedded: { "wp:featuredmedia": { source_url: string }[] };
+  content: { rendered: string };
+  link: string;
+};
+
+const blogPosts = ref<blogPostType[]>();
 
 const isLoading = ref(false);
+
+function truncateString(str: string, maxLength: number) {
+  if (str.length > maxLength) {
+    return str.substring(0, maxLength) + "    ...";
+  } else {
+    return str;
+  }
+}
 
 function fetchBlogPosts() {
   isLoading.value = true;
 
-  const apiUrl = "https://blog.theniftycollective.com/wp-json/wp/v2/posts?_embed";
+  const apiUrl =
+    "https://blog.theniftycollective.com/wp-json/wp/v2/posts?_embed";
 
   axios
     .get(apiUrl)
@@ -42,24 +60,33 @@ onMounted(() => {
 
     <div v-else>
       <div v-for="blog in blogPosts" :key="blog.id" class="mb-8">
-        <div class="lg:px-4">
-          <div>
-            <h1 class="mb-4 text-justify text-xl font-bold">
-              {{ blog.title.rendered }}
-            </h1>
-            <img :src="blog._embedded['wp:featuredmedia'][0].source_url" alt="Featured Image">
+        <div class="grid gap-x-4 md:grid-cols-5">
+          <div class="col-span-2">
+            <Image
+              class="w-80 object-cover object-center"
+              alt="Featured Image"
+              :url="blog._embedded['wp:featuredmedia'][0].source_url"
+            />
           </div>
-
-          <div
-            class="blog-content text-justify text-base"
-            v-html="blog.content.rendered"
-          ></div>
+          <div class="col-span-3">
+            <h1 class="text-xl font-extrabold" v-text="blog.title.rendered" />
+            <div
+              class="blog-content text-justify text-base"
+              v-html="truncateString(blog.content.rendered, 200)"
+            ></div>
+            <NuxtLink
+              class="py-2 text-sm text-primary-400"
+              target="_blank"
+              :to="blog.link"
+            >
+              Read Article
+            </NuxtLink>
+          </div>
         </div>
       </div>
-    </div> 
+    </div>
   </div>
 </template>
-
 
 <style scoped>
 .blog-content {
