1522f8538e0c51536d0abb7ac20dc75a4001a998
alligator33 <>
2023-06-28 16:08:13 +0100

    can fetch featured image

---
+++ components/utils/PreLoader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center">
+    <div class="p-20">
+      <i class="fas fa-spinner fa-spin text-3xl"></i>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
+
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,58 +1,65 @@
+<script lang="ts" setup>
+import PreLoader from "@/components/utils/PreLoader.vue";
+ definePageMeta({
+  name: "blog",
+}); 
+
+import axios from "axios";
+
+const blogPosts = ref([]);
+
+const isLoading = ref(false);
+
+function fetchBlogPosts() {
+  isLoading.value = true;
+
+  const apiUrl = "https://blog.theniftycollective.com/wp-json/wp/v2/posts?_embed";
+
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      blogPosts.value = response.data;
+      isLoading.value = false;
+    })
+    .catch((error) => {
+      console.error("Error fetching blog posts:", error);
+      isLoading.value = false;
+    });
+}
+
+onMounted(() => {
+  fetchBlogPosts();
+});
+</script>
+
 <template>
   <div class="container mx-auto px-4">
     <h2 class="theme-text mb-4 py-6 text-center text-2xl font-bold">
       Latest Blog Posts
     </h2>
 
-    <div v-for="blog in blogPosts" :key="blog.id" class="mb-8">
-      <div class="lg:px-4">
-        <div>
-          <h1 class="mb-4 text-justify text-xl font-bold">
-            {{ blog.title.rendered }}
-          </h1>
-        </div>
+    <PreLoader v-if="isLoading"></PreLoader>
 
-        <div
-          class="blog-content text-justify text-base"
-          v-html="blog.content.rendered"
-        ></div>
+    <div v-else>
+      <div v-for="blog in blogPosts" :key="blog.id" class="mb-8">
+        <div class="lg:px-4">
+          <div>
+            <h1 class="mb-4 text-justify text-xl font-bold">
+              {{ blog.title.rendered }}
+            </h1>
+            <img :src="blog._embedded['wp:featuredmedia'][0].source_url" alt="Featured Image">
+          </div>
+
+          <div
+            class="blog-content text-justify text-base"
+            v-html="blog.content.rendered"
+          ></div>
+        </div>
       </div>
-    </div>
+    </div> 
   </div>
 </template>
 
-<script>
-definePageMeta({
-  name: "blog",
-});
-
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      blogPosts: [],
-    };
-  },
-  mounted() {
-    this.fetchBlogPosts();
-  },
-  methods: {
-    fetchBlogPosts() {
-      const apiUrl = "https://blog.theniftycollective.com/wp-json/wp/v2/posts";
-
-      axios
-        .get(apiUrl)
-        .then((response) => {
-          this.blogPosts = response.data;
-        })
-        .catch((error) => {
-          console.error("Error fetching blog posts:", error);
-        });
-    },
-  },
-};
-</script>
 
 <style scoped>
 .blog-content {
